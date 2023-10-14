9a17a8e2e02ee948b9ec36ea9ca6ed82c964d4ca
Akanksha-Maurya <97236755+Akanksha-Maurya@users.noreply.github.com>
2023-10-14 17:01:19 +0530

    Add Initial Commit

---
+++ Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using MovieStoreMvc.Models.DTO;
+
+namespace MovieStoreMvc.Repositories.Abstract
+{
+    public interface IUserAuthenticationService
+    {
+        Task<Status> LoginAsync(Login model);
+        Task LogoutAsync();
+        Task<Status> RegisterAsync(Registeration model);
+        //Task<Status> ChangePasswordAsync(ChangePasswordModel model, string username);
+    }
+}
